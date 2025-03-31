75bd14688005e13f8491afad9db4121dfacfa94f
Oyebola Ebenezer Ayomikun <ebenezeroyebola2004@gmail.com>
2025-03-31 02:11:36 +0100

    Add files via upload

---
+++ microservice.py
@@ -0,0 +1,22 @@
+import os
+os.system("pip install requirements.txt")
+
+import io
+from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+import verify
+
+app = FastAPI()
+
+app.add_middleware(CORSMiddleware,
+                   allow_origins = ["*"],
+                   allow_credentials = True,
+                   allow_methods = ["*"],
+                   allow_headers = ["*"])
+
+@app.post("/verify-id-owner/")
+async def score(id: UploadFile = File(...), selfie: UploadFile = File(...)):
+  id = await id.read()
+  selfie = await selfie.read()
+  score = verify.compare(id, selfie)
+  return score
